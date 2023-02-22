43825cc631ea718604a95c173e350f34e1b9fb81
adri-12 <adrian_cosma97@yahoo.ro>
2023-02-22 07:44:44 +0200

    Added style to Addpost Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './styles/App.scss';
 import Navbar from "./components/Navbar.js";
+import NewestArticles from './components/NewestArticles';
+import TrendingArticles from './components/TrendingArticles';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <div className='App'>
+      <NewestArticles />
+      <div className='middle-section'>
+        <h1>Get The Knowledge You Need</h1>
+        <h2>Click Bellow To Get Started.</h2>
+        <button>YOUR JOURNEY STARTS HERE</button>
+      </div>
+      <TrendingArticles />
+      </div>
+      </>
   );
 }
 
--- src/pages/Addpost.js
+++ src/pages/Addpost.js
@@ -2,38 +2,72 @@ import React from "react";
 import NewestArticles from "../components/NewestArticles";
 import TrendingArticles from "../components/TrendingArticles";
 
-import '../styles/App.scss';
-import '../styles/add-post.scss';
-
+import "../styles/App.scss";
+import "../styles/add-post.scss";
 
 function Addpost() {
-    return ( 
-      <>
+  return (
+    <>
       <div className="page-layout">
-      <NewestArticles />
-      <div className="add-post"> 
-     <h1>Write An Article</h1>
-     <form action="create-a-post" method="post">
-<ul>
-  <li>
-    <label htmlFor="title">Article Title:</label>
-    <input type="text" id="title" name="article-title" />
-  </li>
-  <li>
-    <label htmlFor="description">Description:</label>
-    <input type="text" id="description" name="article-description" />
-  </li>
-  <li>
-    <label htmlFor="article">The Article:</label>
-    <textarea name="content" id="article-content" cols="30" rows="10" placeholder="Type here the content for your article..."></textarea>
-  </li>
-</ul>
-     </form>
+        <NewestArticles />
+        <div className="add-post">
+          <h1>Write An Article</h1>
+          <div className="form-container">
+            <div className="logo-text">
+              <img src="\logo512.png" alt="React" />
+              <span>Get Inspired</span>
+            </div>
+            <form action="create-a-post" method="post">
+              <ul>
+                <li>
+                  <label htmlFor="title">Article Title:</label>
+                  <input
+                    className="m-top-10"
+                    type="text"
+                    id="title"
+                    name="article-title"
+                  />
+                </li>
+                <li>
+                  <label htmlFor="description">Description:</label>
+                  <input
+                    className="m-top-10"
+                    type="text"
+                    id="description"
+                    name="article-description"
+                  />
+                </li>
+                <li>
+                  <label htmlFor="article">The Article:</label>
+                  <textarea
+                    name="content"
+                    id="article-content"
+                    className="m-top-10"
+                    cols="30"
+                    rows="10"
+                    placeholder="Type here the content for your article..."
+                  ></textarea>
+                </li>
+                <div className="select-category m-top-20">
+                  <span>Select your article category: </span>
+                  <select name="categories" id="categories">
+                    <option>Fitness</option>
+                    <option>Nutrition</option>
+                    <option>Physiology</option>
+                    <option>Other</option>
+                  </select>
+                </div>
+              </ul>
+              <button type="submit" className="btn-submit m-top-20">
+                Create Article
+              </button>
+            </form>
+          </div>
+        </div>
+        <TrendingArticles />
       </div>
-      <TrendingArticles />
-      </div>
-      </>
-    );
-  }
+    </>
+  );
+}
 
-  export default Addpost;
+export default Addpost;
