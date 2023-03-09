5e6e859a738175816e647f1bdebff7b0e82d7f93
adri-12 <adrian_cosma97@yahoo.ro>
2023-03-09 09:48:10 +0200

    Added style for a responsive Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import { Link } from 'react-router-dom';
 import '../styles/navbar.scss';
-
+import { useState } from 'react';
 
 const Navbar = () => {
+
     return ( 
         <nav className='navbar'>
+            <div className='logo-container'>
             <Link to="/" >
                  <img className="logo" src="/logo512.png" alt="React" />
             </Link>
+            </div>
             <div className="links">
             <Link to="/">Home</Link>
             <Link to="/create">New Article</Link>
+            </div>
+            <div className='burger'>
+                <div className='line1'></div>
+                <div className='line2'></div>
+                <div className='line3'></div>
             </div>
         </nav>
      );
